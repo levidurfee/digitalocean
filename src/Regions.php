5e8b958bd3acd9780cd8a18756daff94f5425e0a
Levi Durfee <levi.durfee@gmail.com>
2017-02-17 16:02:36 -0500

    [Regions] add resource/request

---
+++ src/Regions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace wappr\digitalocean;
+
+use wappr\digitalocean\Requests\Regions\ListAllRegionsRequest;
+
+class Regions extends Resources
+{
+    public function listAll(ListAllRegionsRequest $listAllRegionsRequest)
+    {
+
+    }
+}
---
+++ src/Requests/Regions/ListAllRegionsRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace wappr\digitalocean\Requests\Regions;
+
+use wappr\digitalocean\RequestContract;
+
+class ListAllRegionsRequest extends RequestContract
+{
+
+}
