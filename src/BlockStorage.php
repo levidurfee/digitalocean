6a90691c1df48fab4e8a8ce93172ac452647b5d0
Levi Durfee <levi.durfee@gmail.com>
2017-02-19 18:27:58 -0500

    [BlockStorage] start working on tests

--- src/BlockStorage.php
+++ src/BlockStorage.php
@@ -21,10 +21,21 @@ class BlockStorage extends Resources
      */
     protected $clientAdapter;
 
+    /**
+     * API Endpoint.
+     *
+     * @var string
+     */
     private $endpoint = 'volumes';
 
+    /**
+     * @param ListAllBlockStorageRequest $listAllBlockStorageRequest
+     *
+     * @return mixed|null|\Psr\Http\Message\ResponseInterface
+     */
     public function listAll(ListAllBlockStorageRequest $listAllBlockStorageRequest)
     {
+        return $this->clientAdapter->get($this->endpoint, $listAllBlockStorageRequest);
     }
 
     public function create(CreateBlockStorageRequest $createBlockStorageRequest)
---
+++ tests/BlockStorageTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace wappr\digitalocean;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use wappr\digitalocean\Requests\BlockStorage\ListAllBlockStorageRequest;
+
+class BlockStorageTest extends \PHPUnit_Framework_TestCase
+{
+    protected $client;
+
+    public function setUp()
+    {
+        $mock = new MockHandler([
+            new Response(200, ['X-Foo' => 'Bar']),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->client = new Client(['handler' => $handler]);
+    }
+
+    public function testListAll()
+    {
+        $listAll = new ListAllBlockStorageRequest;
+        $blockStorage = new BlockStorage($this->client);
+        $result = $blockStorage->listAll($listAll);
+        $this->assertEquals($result->getStatusCode(), 200);
+    }
+}
