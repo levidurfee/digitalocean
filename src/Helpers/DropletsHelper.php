0a6be1338eda6c90feb920e383e39caec884e66b
Levi Durfee <levi.durfee@gmail.com>
2017-02-23 19:13:40 -0500

    [DropletsHelper] remove regions

--- src/Helpers/DropletsHelper.php
+++ src/Helpers/DropletsHelper.php
@@ -4,11 +4,6 @@ namespace wappr\digitalocean\Helpers;
 
 class DropletsHelper
 {
-    /* regions */
-    const REGION_NYC1 = 'nyc1';
-    const REGION_NYC2 = 'nyc2';
-    const REGION_NYC3 = 'nyc3';
-
     /* sizes */
     const SIZE_512 = '512mb';
 
