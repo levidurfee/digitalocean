c52b35abeb33ff99928d1ef1e71af3a2fbf49c2a
Levi Durfee <levi.durfee@gmail.com>
2017-02-27 19:33:23 -0500

    [BlockStorage] use NameRegion trait for delete by name

--- src/Requests/BlockStorage/DeleteByNameBlockStorageRequest.php
+++ src/Requests/BlockStorage/DeleteByNameBlockStorageRequest.php
@@ -3,7 +3,9 @@
 namespace wappr\digitalocean\Requests\BlockStorage;
 
 use wappr\digitalocean\RequestContract;
+use wappr\digitalocean\Requests\BlockStorage\Traits\NameRegion;
 
 class DeleteByNameBlockStorageRequest extends RequestContract
 {
+    use NameRegion;
 }
