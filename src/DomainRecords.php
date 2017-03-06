d2a483450b2679963e7e3bd93a8bd6a0479ec510
Levi Durfee <levi.durfee@gmail.com>
2017-03-06 11:22:23 -0500

    [DomainRecords] start

--- src/DomainRecords.php
+++ src/DomainRecords.php
@@ -10,8 +10,26 @@ use wappr\digitalocean\Requests\DomainRecords\UpdateDomainRecordsRequest;
 
 class DomainRecords extends Resources
 {
+    /**
+     * @var ClientAdapter
+     */
+    private $clientAdapter;
+
+    /**
+     * @var string
+     */
+    private $endpoint = 'domains';
+
+    /**
+     * List all the records for a specific domain.
+     *
+     * @param ListAllDomainRecordsRequest $listAllDomainRecordsRequest
+     *
+     * @return mixed|null|\Psr\Http\Message\ResponseInterface
+     */
     public function listAll(ListAllDomainRecordsRequest $listAllDomainRecordsRequest)
     {
+        return $this->clientAdapter->get($this->endpoint.'/'.$listAllDomainRecordsRequest->domain_name.'/records', $listAllDomainRecordsRequest);
     }
 
     public function create(CreateDomainRecordsRequest $createDomainRecordsRequest)
