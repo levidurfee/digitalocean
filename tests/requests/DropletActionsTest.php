8697a4430386c0b29e86cab5b6a079e5bec336c4
Levi Durfee <levi.durfee@gmail.com>
2017-04-11 21:07:45 -0400

    Format test file (#8)

--- tests/requests/DropletActionsTest.php
+++ tests/requests/DropletActionsTest.php
@@ -7,19 +7,6 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use wappr\digitalocean\Requests\DropletActions\DropletActionsRequest;
-use wappr\digitalocean\Requests\Droplets\CreateDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\CreateMultipleDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\DeleteByTagDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\DeleteDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\ListActionsDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\ListAllDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\ListAllNeighborsDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\ListAvailableKernelsDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\ListBackupsDropletRequest;
-use wappr\digitalocean\Requests\Droplets\ListByTagDropletRequest;
-use wappr\digitalocean\Requests\Droplets\ListNeighborsDropletsRequest;
-use wappr\digitalocean\Requests\Droplets\ListSnapshotsDropletRequest;
-use wappr\digitalocean\Requests\Droplets\RetrieveDropletsRequest;
 
 class DropletActionsTest extends \PHPUnit_Framework_TestCase
 {
@@ -36,12 +23,12 @@ class DropletActionsTest extends \PHPUnit_Framework_TestCase
     public function testSuccessfulRequests()
     {
         $methods = [
-            'enableBackups'
+            'enableBackups',
         ];
 
-        foreach($methods as $method) {
+        foreach ($methods as $method) {
             $mock = new MockHandler([
-                new Response(200)
+                new Response(200),
             ]);
             $handler = HandlerStack::create($mock);
             $client = new Client(['handler' => $handler]);
